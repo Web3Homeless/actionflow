8044e36e05e6670ad341ddcde5621624cdc93cf3
aii23 <a.ivlev.om@gmail.com>
2024-11-17 05:11:01 +0700

    Created transfer triger node

--- src/domain/nodes/transfer-trigger-node.ts
+++ src/domain/nodes/transfer-trigger-node.ts
@@ -1,7 +1,9 @@
+import { toChecksumAddress } from "../format";
 import { type ITriggerNode, type INode, type NodeType } from "../interfaces";
 
 export type TransferTriggerNodeParams = {
   account: string;
+  token: string;
 };
 
 export class TransferTriggerNode implements ITriggerNode {
@@ -13,6 +15,7 @@ export class TransferTriggerNode implements ITriggerNode {
   private nodeParams: TransferTriggerNodeParams;
 
   constructor(params: TransferTriggerNodeParams) {
+    params.token = toChecksumAddress(params.token);
     this.nodeParams = params;
   }
 
@@ -22,33 +25,71 @@ export class TransferTriggerNode implements ITriggerNode {
         function increment() external;
         function getCount() external view returns (uint256);
       }
+
+      interface ITxProver {
+        function verifyCopytrading(Proof calldata data, string calldata txHash, bytes calldata txInput, address txTo, uint256 txValue) external;
+      }
     `;
   }
 
   getContractStateParams(): string {
     return `
-      uint256 public someTransferParam1; 
-      uint256 public someTransferParam2;
+      bytes4 private constant ERC20_TRANSFER_SELECTOR = 0xa9059cbb;
     `;
   }
 
   getContractFunctionCall(): string {
     return `
-      transfer("${this.nodeParams.account}");
+      checkProof(proof, txHash, txInput, txTo, txValue);
+      require(checkERC20Calldata(txInput), "Provided transaction is not ERC20 transfer");
+      require(txTo == ${this.nodeParams.token}, "Provided transaction do not transfer desired tokens");
+
     `;
   }
 
   getContractFunctionDeclaration(): string {
     return `
-      function transfer(address account) public override {
-        someTransferParam1 += 1;
+      function checkERC20Calldata(bytes calldata data) internal view returns (bool) {
+        if (data.length != 68) {
+            return false;
+        }
+
+        bytes4 selector = bytes4(data[:4]);
+
+        // Verify the selector matches the ERC20 transfer function
+        if (selector != ERC20_TRANSFER_SELECTOR) {
+            return false;
+        }
+
+        address to;
+        uint256 amount;
+
+        assembly {
+            // Skip the first 4 bytes (selector) and load the next 32 bytes (address "to")
+            to := calldataload(add(data.offset, 4))
+            // Load the following 32 bytes (uint256 "amount")
+            amount := calldataload(add(data.offset, 36))
+        }
+
+        if (to != address(this)) {
+            return false;
+        }
+
+        return true;
+      }
+      function checkProof(Proof calldata proof, string calldata txHash, bytes calldata txInput,
+        address txTo, uint256 txValue) public {
+        ITxProver(vlayerProver).verifyCopytrading(proof, txHash, txInput, txTo, txValue);
+
+
       }
     `;
   }
 
   getContractEntrypointSignature(): string {
     return `
-      function entrypoint() public override
+      function entrypoint(Proof calldata proof, string calldata txHash, bytes calldata txInput,
+        address txTo, uint256 txValue) public
     `;
   }
 }
