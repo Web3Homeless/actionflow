353ca15bf7b5f8424c61776dc367713bbf954167
Neo-Gar <nvatlecov@mail.ru>
2024-11-17 06:17:51 +0700

    Add optimismSepolia

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,11 +1,11 @@
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import { cookieStorage, createStorage } from "wagmi";
-import { arbitrumSepolia, sepolia, polygonZkEvm } from "@reown/appkit/networks";
+import { arbitrumSepolia, sepolia, polygonZkEvm, optimismSepolia } from "@reown/appkit/networks";
 
 // Get projectId from https://cloud.reown.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 
-export const networks = [arbitrumSepolia, sepolia, polygonZkEvm];
+export const networks = [arbitrumSepolia, sepolia, polygonZkEvm, optimismSepolia];
 
 if (!projectId) throw new Error("Project ID is not defined");
 
