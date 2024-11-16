67b6b8ac6287eb40e94fe0a139da38421e993bbe
Ilya Klimenko <sn0wy-0wl@yandex.ru>
2024-11-16 15:52:41 +0300

    Added deployment button and dialog

--- src/components/widgets/CodeEditor/CodeEditor.tsx
+++ src/components/widgets/CodeEditor/CodeEditor.tsx
@@ -1,7 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { solidity } from "@replit/codemirror-lang-solidity";
 import { useWorkflowStore } from "@/store/workflowStore";
+import { deployContract } from "@wagmi/core";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 
 type Props = {};
@@ -20,7 +32,23 @@ export default function CodeEditor({}: Props) {
         extensions={extensions}
         onChange={(value) => setCode(value)}
       />
-      <Button>Deploy Contract</Button>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="outline">Show Dialog</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Вы уверены?</AlertDialogTitle>
+            <AlertDialogDescription>
+              Если эту хуйню задеплоить и вас заскамят - мы не виноваты
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Ну нахуй</AlertDialogCancel>
+            <AlertDialogAction>Похуй</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
